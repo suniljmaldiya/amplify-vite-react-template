c5f16211e6a4ca11fcc96945999eb5129ffb60f7
Sunil Saini <72140848+suniljmaldiya@users.noreply.github.com>
2026-02-12 10:10:17 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
   }
   return (
     <main>
-      <h1>My todos</h1>
+      <h1>{user?.signInDetails?.loginId}'s todos</h1>
       <button onClick={createTodo}>+ new</button>
       
       <ul>
