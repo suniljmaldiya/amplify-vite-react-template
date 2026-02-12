c43cc87b1e8b4df473f004d31cea71390e70d4fd
Sunil Saini <72140848+suniljmaldiya@users.noreply.github.com>
2026-02-12 10:36:06 +0530

    Update resource.ts

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -19,11 +19,9 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
+    // This tells the data client in your app (generateClient())
+    // to sign API requests with the user authentication token.
     defaultAuthorizationMode: 'userPool',
-    // API Key is used for a.allow.public() rules
-    apiKeyAuthorizationMode: {
-      expiresInDays: 30,
-    },
   },
 });
 
